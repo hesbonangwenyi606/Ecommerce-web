a4f53fee706731c502efae028a348e1b2e758a8c
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-27 23:45:23 +0300

    updated

---
+++ src/components/Footer/Careers.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Careers = () => (
+  <div className="text-gray-400 text-sm space-y-1">
+    <p>We are always looking for talented people to join our team.</p>
+    <p>Send your CV to 📧 careers@luxystore.com</p>
+    <p>Explore open positions on our website.</p>
+  </div>
+);
