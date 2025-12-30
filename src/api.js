90c14dd1709f923bbe21219aef1154ee1be3817e
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-30 12:41:30 +0300

    updated

--- src/api.js
+++ src/api.js
@@ -1,3 +1,35 @@
+import axios from "axios";
+
+// Create Axios instance
+const API = axios.create({
+  baseURL: "http://localhost:5000/api",
+});
+
+// Add a request interceptor to include token
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+// Auth
 export const registerUser = (data) => API.post("/auth/register", data);
 export const loginUser = (data) => API.post("/auth/login", data);
 export const getCurrentUser = () => API.get("/auth/me");
+
+// Products
+export const getProducts = () => API.get("/products");
+export const getProduct = (id) => API.get(`/products/${id}`);
+
+// Orders
+export const createOrder = (orderData) => API.post("/orders", orderData);
+export const getUserOrders = () => API.get("/orders/user");
+
+// Payments
+export const createCheckoutSession = (data) =>
+  API.post("/payments/create-checkout-session", data);
+
