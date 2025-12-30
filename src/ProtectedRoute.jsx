71c9fa0d3bea915b38eb8065f089c00da56f8a7b
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-30 13:15:32 +0300

    updated

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,8 +1,13 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ user, children }) {
-  if (!user) {
-    return <Navigate to="/login" replace />;
+const ProtectedRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to="/login" />;
   }
   return children;
-}
+};
+
+export default ProtectedRoute;
+
