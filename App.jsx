6ef6e20f09e60a0894806facc6d9e228001e6b7a
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-30 11:30:05 +0300

    updated

--- App.jsx
+++ App.jsx
@@ -1,14 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getProducts } from "./api";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     getProducts()
-      .then(res => console.log(res.data))
+      .then(res => setProducts(res.data))
       .catch(err => console.error(err));
   }, []);
 
-  return <div>Open console to see products from backend</div>;
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
+
