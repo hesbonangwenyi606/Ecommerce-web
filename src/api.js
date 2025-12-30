e9f3b5cbe39014514e9ae3c81e5f98b0cac2a8a8
Hesbon Angwenyi <hesbonmanyinsa96@gmail.com>
2025-12-30 12:25:13 +0300

    updated

---
+++ src/api.js
@@ -0,0 +1,3 @@
+export const registerUser = (data) => API.post("/auth/register", data);
+export const loginUser = (data) => API.post("/auth/login", data);
+export const getCurrentUser = () => API.get("/auth/me");
